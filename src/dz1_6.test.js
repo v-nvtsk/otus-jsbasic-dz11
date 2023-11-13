36acc4ed6d510c9f7eb4d1d8eb4da14b38815ee9
v-nvtsk <v@nvtsk.ru>
2023-11-13 16:38:07 +0300

    fix dz1_6.test.js remove common describe()

--- src/dz1_6.test.js
+++ src/dz1_6.test.js
@@ -5,179 +5,178 @@ import {
   dz4, dz5, dz6_diff, dz6_isWord, dz6_pow
 } from './dz1_6';
 
-describe("dz 1 to 6 test suite", () => {
-  beforeEach(() => {
-    console.log = jest.fn();
-  });
+beforeEach(() => {
+  console.log = jest.fn();
+});
 
-  afterEach(() => {
-    console.log.mockRestore();
-  });
+afterEach(() => {
+  console.log.mockRestore();
+});
 
-  describe("dz1", () => {
+describe("dz1", () => {
 
-    it('should be a function', () => {
-      expect(dz1_1).toBeInstanceOf(Function);
-      expect(dz1_2).toBeInstanceOf(Function);
-      expect(dz1_3).toBeInstanceOf(Function);
-    })
+  it('should be a function', () => {
+    expect(dz1_1).toBeInstanceOf(Function);
+    expect(dz1_2).toBeInstanceOf(Function);
+    expect(dz1_3).toBeInstanceOf(Function);
+  })
 
-    describe('Test dz1_1', () => {
-      /*
+  describe('Test dz1_1', () => {
+    /*
         1.В переменных a и b хранятся числа. Написать
       программу, которая выводит в консоль произведение
       и сумму этих чисел.
       */
-      const testData = [
-        { a: 6, b: 5, result: '30 11' },
-        { a: -2, b: 3, result: '-6 1' },
-        { a: 'text', b: 'text', result: 'Неверный ввод' }
-      ]
-      testData.forEach(({ a, b, result }) => {
-        it(`should return '${result}' for a = ${a}, b = ${b}`, () => {
+    const testData = [
+      { a: 6, b: 5, result: '30 11' },
+      { a: -2, b: 3, result: '-6 1' },
+      { a: 'text', b: 'text', result: 'Неверный ввод' }
+    ]
+    testData.forEach(({ a, b, result }) => {
+      it(`should return '${result}' for a = ${a}, b = ${b}`, () => {
 
-          dz1_1(a, b);
-          expect(console.log).toHaveBeenCalledWith(result);
-        });
-      })
+        dz1_1(a, b);
+        expect(console.log).toHaveBeenCalledWith(result);
+      });
     })
+  })
 
-    describe('Test dz1_2', () => {
-      /*
+  describe('Test dz1_2', () => {
+    /*
         2.В двух переменных хранятся строки символов.
         Написать программу, которая выведет в консоль
         суммарное количество символов в обоих строках.
      */
-      const testData = [
-        { a: "a", b: "b", result: "2" },
-        { a: "string1", b: "lorem", result: "12" },
-        { a: "", b: "", result: "0" },
-        { a: undefined, b: undefined, result: "0" }
-      ]
-      testData.forEach(({ a, b, result }) => {
+    const testData = [
+      { a: "a", b: "b", result: "2" },
+      { a: "string1", b: "lorem", result: "12" },
+      { a: "", b: "", result: "0" },
+      { a: undefined, b: undefined, result: "0" }
+    ]
+    testData.forEach(({ a, b, result }) => {
 
-        it(`should return '${result}' for a = ${a}, b = ${b}`, () => {
+      it(`should return '${result}' for a = ${a}, b = ${b}`, () => {
 
-          dz1_2(a, b);
-          expect(console.log).toHaveBeenCalledWith(result);
-        });
-      })
-
+        dz1_2(a, b);
+        expect(console.log).toHaveBeenCalledWith(result);
+      });
     })
 
-    describe('Test dz1_3', () => {
-      /* 
+  })
+
+  describe('Test dz1_3', () => {
+    /* 
         3.*Написать программу, которая запрашивает у
         пользователя ввод трёхзначного числа, а потом
         выводит в консоль сумму цифр введённого числа.
       */
-      const testData = [
-        { a: '367', result: '16' },
-        { a: '99', result: 'Неверный ввод' },
-        { a: '1234', result: 'Неверный ввод' },
-        { a: '', result: 'Неверный ввод' },
-        { a: 'text', result: 'Неверный ввод' }
-      ]
-      testData.forEach(({ a, result }) => {
-        it(`should return '${result}' for a = ${a}`, () => {
-          dz1_3(a);
-          expect(console.log).toHaveBeenCalledWith(result);
-        });
+    const testData = [
+      { a: '367', result: '16' },
+      { a: '99', result: 'Неверный ввод' },
+      { a: '1234', result: 'Неверный ввод' },
+      { a: '', result: 'Неверный ввод' },
+      { a: 'text', result: 'Неверный ввод' }
+    ]
+    testData.forEach(({ a, result }) => {
+      it(`should return '${result}' for a = ${a}`, () => {
+        dz1_3(a);
+        expect(console.log).toHaveBeenCalledWith(result);
       });
     });
   });
+});
 
-  describe("dz2", () => {
-    it('should be a function', () => {
-      expect(dz2_1).toBeInstanceOf(Function);
-      expect(dz2_2).toBeInstanceOf(Function);
-      expect(dz2_3).toBeInstanceOf(Function);
-    })
-    describe('Test dz2_1', () => {
-      /*
+describe("dz2", () => {
+  it('should be a function', () => {
+    expect(dz2_1).toBeInstanceOf(Function);
+    expect(dz2_2).toBeInstanceOf(Function);
+    expect(dz2_3).toBeInstanceOf(Function);
+  })
+  describe('Test dz2_1', () => {
+    /*
         1.В переменных a и b хранятся числа. Вывести в
         консоль наибольшее из них
      */
-      const testData = [
-        { a: 1, b: 2, result: "2" },
-        { a: 2, b: 1, result: "2" },
-        { a: '', b: '', result: 'Неверный ввод' },
-        { a: undefined, b: undefined, result: 'Неверный ввод' },
-        { a: 'text', b: 'text', result: 'Неверный ввод' }
-      ]
+    const testData = [
+      { a: 1, b: 2, result: "2" },
+      { a: 2, b: 1, result: "2" },
+      { a: '', b: '', result: 'Неверный ввод' },
+      { a: undefined, b: undefined, result: 'Неверный ввод' },
+      { a: 'text', b: 'text', result: 'Неверный ввод' }
+    ]
 
-      testData.forEach(({ a, b, result }) => {
-        it(`should return '${result}' for a = ${a}, b = ${b}`, () => {
-          dz2_1(a, b)
-          expect(console.log).toHaveBeenCalledWith(result);
-        })
+    testData.forEach(({ a, b, result }) => {
+      it(`should return '${result}' for a = ${a}, b = ${b}`, () => {
+        dz2_1(a, b)
+        expect(console.log).toHaveBeenCalledWith(result);
       })
-    });
+    })
+  });
 
-    describe('Test dz2_2', () => {
-      /*
+  describe('Test dz2_2', () => {
+    /*
         2.Запросить у пользователя ввод числа от 1 до 12.
         Вывести в консоль название месяца, соответствующее
         этому числу (1 — январь, 2 — февраль и т.д.).
      */
-      const testData = [
-        { a: '1', result: 'январь' },
-        { a: '12', result: 'декабрь' },
-        { a: 'text', result: 'Неверный ввод' }
-      ]
+    const testData = [
+      { a: '1', result: 'январь' },
+      { a: '12', result: 'декабрь' },
+      { a: 'text', result: 'Неверный ввод' }
+    ]
 
-      testData.forEach(({ a, result }) => {
-        it(`should return '${result}' for a = ${a}`, () => {
-          dz2_2(a)
-          expect(console.log).toHaveBeenCalledWith(result);
-        })
+    testData.forEach(({ a, result }) => {
+      it(`should return '${result}' for a = ${a}`, () => {
+        dz2_2(a)
+        expect(console.log).toHaveBeenCalledWith(result);
       })
-    });
+    })
+  });
 
-    describe('Test dz2_3', () => {
-      /*
+  describe('Test dz2_3', () => {
+    /*
         *В переменных circle и square хранятся площади круга
         и квадрата соответственно. Написать программу,
         которая определяет, поместится ли круг в квадрат.
      */
 
-      const testData = [
-        { circle: 12.56, square: 16, result: "YES" },
-        { circle: 12.57, square: 16, result: "NO" },
-        { circle: 3.14, square: 4, result: "YES" },
-        { circle: 'text', square: 'text', result: 'Неверный ввод' },
-        { circle: undefined, square: undefined, result: 'Неверный ввод' },
-        { circle: '', square: '', result: 'Неверный ввод' },
-        { circle: 12.56, square: undefined, result: "Неверный ввод" }
-      ]
+    const testData = [
+      { circle: 12.56, square: 16, result: "YES" },
+      { circle: 12.57, square: 16, result: "NO" },
+      { circle: 3.14, square: 4, result: "YES" },
+      { circle: 'text', square: 'text', result: 'Неверный ввод' },
+      { circle: undefined, square: undefined, result: 'Неверный ввод' },
+      { circle: '', square: '', result: 'Неверный ввод' },
+      { circle: 12.56, square: undefined, result: "Неверный ввод" }
+    ]
 
-      testData.forEach(({ circle, square, result }) => {
-        it(`should return '${result}' for circle = ${circle}, square = ${square}`, () => {
-          dz2_3(circle, square)
-          expect(console.log).toHaveBeenCalledWith(result);
-        })
+    testData.forEach(({ circle, square, result }) => {
+      it(`should return '${result}' for circle = ${circle}, square = ${square}`, () => {
+        dz2_3(circle, square)
+        expect(console.log).toHaveBeenCalledWith(result);
       })
-    });
-  });
-
-  describe("dz3", () => {
-    it('should be a function', () => {
-      expect(dz3_1).toBeInstanceOf(Function);
-      expect(dz3_2).toBeInstanceOf(Function);
-      expect(dz3_3).toBeInstanceOf(Function);
     })
-    test('Test dz3_1', () => {
-      /*
+  });
+});
+
+describe("dz3", () => {
+  it('should be a function', () => {
+    expect(dz3_1).toBeInstanceOf(Function);
+    expect(dz3_2).toBeInstanceOf(Function);
+    expect(dz3_3).toBeInstanceOf(Function);
+  })
+  test('Test dz3_1', () => {
+    /*
         1.Вывести в консоль сумму всех целых чисел от 50 до 100.
      */
 
 
-      dz3_1();
-      expect(console.log).toHaveBeenCalledWith(`${(100 * (100 + 1) / 2 - 50 * (50 + 1) / 2 + 50)}`);
-    });
+    dz3_1();
+    expect(console.log).toHaveBeenCalledWith(`${(100 * (100 + 1) / 2 - 50 * (50 + 1) / 2 + 50)}`);
+  });
 
-    test('Test dz3_2', () => {
-      /*
+  test('Test dz3_2', () => {
+    /*
         2.Вывести в консоль таблицу умножения на 7.
         7 x 1 = 7
         7 x 2 = 14
@@ -185,59 +184,59 @@ describe("dz 1 to 6 test suite", () => {
         7 x 9 = 63
      */
 
-      const testData = ['7 x 1 = 7',
-        '7 x 2 = 14',
-        '7 x 3 = 21',
-        '7 x 4 = 28',
-        '7 x 5 = 35',
-        '7 x 6 = 42',
-        '7 x 7 = 49',
-        '7 x 8 = 56',
-        '7 x 9 = 63']
-      dz3_2();
-      expect(console.log).toHaveBeenCalledTimes(9);
-      for (let i = 1; i < 10; i++) {
-        expect(console.log).toHaveBeenNthCalledWith(i, testData[i - 1]);
-      }
-    });
+    const testData = ['7 x 1 = 7',
+      '7 x 2 = 14',
+      '7 x 3 = 21',
+      '7 x 4 = 28',
+      '7 x 5 = 35',
+      '7 x 6 = 42',
+      '7 x 7 = 49',
+      '7 x 8 = 56',
+      '7 x 9 = 63']
+    dz3_2();
+    expect(console.log).toHaveBeenCalledTimes(9);
+    for (let i = 1; i < 10; i++) {
+      expect(console.log).toHaveBeenNthCalledWith(i, testData[i - 1]);
+    }
+  });
 
-    describe('Test dz3_3', () => {
-      /*
+  describe('Test dz3_3', () => {
+    /*
         *Запросить у пользователя ввод числа N. Вывести в
         консоль среднее арифметическое всех нечётных
         чисел от 1 до N.
      */
-      const sum = (n) => (Math.floor((n + 1) / 2)) ** 2;
-      const average = (n) => sum(n) / Math.ceil(n / 2)
+    const sum = (n) => (Math.floor((n + 1) / 2)) ** 2;
+    const average = (n) => sum(n) / Math.ceil(n / 2)
 
-      const testData = [
-        { n: 'text', result: 'Неверный ввод' },
-        { n: undefined, result: 'Неверный ввод' },
-        { n: '', result: 'Неверный ввод' },
-        { n: 0, result: 'Неверный ввод' },
-        { n: 1, result: 1 },
-        { n: 2, result: 1 },
-        { n: 3, result: 2 },
-        { n: 4, result: 2 },
-        { n: 15, result: average(15) },
-        { n: 16, result: average(15) }
-      ]
+    const testData = [
+      { n: 'text', result: 'Неверный ввод' },
+      { n: undefined, result: 'Неверный ввод' },
+      { n: '', result: 'Неверный ввод' },
+      { n: 0, result: 'Неверный ввод' },
+      { n: 1, result: 1 },
+      { n: 2, result: 1 },
+      { n: 3, result: 2 },
+      { n: 4, result: 2 },
+      { n: 15, result: average(15) },
+      { n: 16, result: average(15) }
+    ]
 
-      testData.forEach(({ n, result }) => {
-        it(`should return '${result}' for n = ${n}`, () => {
-          dz3_3(n)
-          expect(console.log).toHaveBeenCalledWith(result);
-        })
+    testData.forEach(({ n, result }) => {
+      it(`should return '${result}' for n = ${n}`, () => {
+        dz3_3(n)
+        expect(console.log).toHaveBeenCalledWith(result);
       })
-    });
-  })
-
-  describe("dz4", () => {
-    it('should be a function', () => {
-      expect(dz4).toBeInstanceOf(Function);
     })
-    describe('Test dz4', () => {
-      /*
+  });
+})
+
+describe("dz4", () => {
+  it('should be a function', () => {
+    expect(dz4).toBeInstanceOf(Function);
+  })
+  describe('Test dz4', () => {
+    /*
         Создайте объект user, содержащий поле name со
         значением ‘John’.
         1.Запросить у пользователя ввод числа. Записать
@@ -250,30 +249,30 @@ describe("dz 1 to 6 test suite", () => {
         должны совпадать с названиями полей.
      */
 
-      const testData = [
-        { n: 39, result: ['John', 'admin', 39] },
-        { n: 40, result: ['John', 'admin', 40] },
-        { n: 'text', result: ['Неверный ввод'] }
-      ]
+    const testData = [
+      { n: 39, result: ['John', 'admin', 39] },
+      { n: 40, result: ['John', 'admin', 40] },
+      { n: 'text', result: ['Неверный ввод'] }
+    ]
 
-      testData.forEach(({ n, result }) => {
-        it(`should return '${result}' for n = ${n}`, () => {
-          dz4(n)
-          expect(console.log).toHaveBeenCalledTimes(result.length);
-          for (let i = 0; i < result.length; i++) {
-            expect(console.log).toHaveBeenNthCalledWith(i + 1, result[i]);
-          }
-        })
+    testData.forEach(({ n, result }) => {
+      it(`should return '${result}' for n = ${n}`, () => {
+        dz4(n)
+        expect(console.log).toHaveBeenCalledTimes(result.length);
+        for (let i = 0; i < result.length; i++) {
+          expect(console.log).toHaveBeenNthCalledWith(i + 1, result[i]);
+        }
       })
-    });
-  });
-
-  describe("dz5", () => {
-    it('should be a function', () => {
-      expect(dz5).toBeInstanceOf(Function);
     })
-    describe('Test dz5', () => {
-      /*
+  });
+});
+
+describe("dz5", () => {
+  it('should be a function', () => {
+    expect(dz5).toBeInstanceOf(Function);
+  })
+  describe('Test dz5', () => {
+    /*
         Создайте массив целых чисел из 10 элементов.
         1.Выведите в консоль сумму всех элементов массива.
         2.Создайте новый массив на основе исходного, в
@@ -285,92 +284,90 @@ describe("dz 1 to 6 test suite", () => {
         наименьший элементы исходного массива.
      */
 
-      const sum = 1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19
+    const sum = 1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19
 
-      const testData = [
-        {
-          input: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
-          result: [sum,
-            '2 6 10 14 18 22 26 30 34 38',
-            '38 2'
-          ]
+    const testData = [
+      {
+        input: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
+        result: [sum,
+          '2 6 10 14 18 22 26 30 34 38',
+          '38 2'
+        ]
+      }
+    ]
+    testData.forEach(({ input, result }) => {
+      it(`should return '${result.join(' | ')}' for input = ${input}`, () => {
+        dz5(...input)
+        expect(console.log).toHaveBeenCalledTimes(result.length);
+        for (let i = 0; i < result.length; i++) {
+          expect(console.log).toHaveBeenNthCalledWith(i + 1, result[i]);
         }
-      ]
-      testData.forEach(({ input, result }) => {
-        it(`should return '${result.join(' | ')}' for input = ${input}`, () => {
-          dz5(...input)
-          expect(console.log).toHaveBeenCalledTimes(result.length);
-          for (let i = 0; i < result.length; i++) {
-            expect(console.log).toHaveBeenNthCalledWith(i + 1, result[i]);
-          }
-        })
       })
-    });
-  })
-
-  describe("dz6", () => {
-    it('should be a function', () => {
-      expect(dz6_diff).toBeInstanceOf(Function);
-      expect(dz6_isWord).toBeInstanceOf(Function);
-      expect(dz6_pow).toBeInstanceOf(Function);
     })
-    describe('Test dz6_diff', () => {
-      /*
+  });
+})
+
+describe("dz6", () => {
+  it('should be a function', () => {
+    expect(dz6_diff).toBeInstanceOf(Function);
+    expect(dz6_isWord).toBeInstanceOf(Function);
+    expect(dz6_pow).toBeInstanceOf(Function);
+  })
+  describe('Test dz6_diff', () => {
+    /*
         1.Напишите функцию diff, которая получает в качестве
         параметров 2 числа и возвращает разницу между
         наибольшим и наименьшим.
      */
-      const testData = [
-        { a: 1, b: 5, result: 4 },
-        { a: 5, b: 1, result: 4 },
-        { a: 1, b: 1, result: 0 },
-        { a: -1, b: 5, result: 6 }
-      ]
+    const testData = [
+      { a: 1, b: 5, result: 4 },
+      { a: 5, b: 1, result: 4 },
+      { a: 1, b: 1, result: 0 },
+      { a: -1, b: 5, result: 6 }
+    ]
 
-      testData.forEach(({ a, b, result }) => {
-        it(`should return '${result}' for a = ${a}, b = ${b}`, () => {
-          dz6_diff(a, b)
-          expect(console.log).toHaveBeenCalledWith(result);
-        })
+    testData.forEach(({ a, b, result }) => {
+      it(`should return '${result}' for a = ${a}, b = ${b}`, () => {
+        dz6_diff(a, b)
+        expect(console.log).toHaveBeenCalledWith(result);
       })
-    });
-
-    describe('Test dz6_isWord', () => {
-      /*
-        2.Напишите функцию isWord, которая принимает на
-        вход текстовую строку. Функция возвращает true, если
-        строка состоит из одного слова и false, если из
-        нескольких.
-     */
-      const testData = [
-        { input: 'word', result: true },
-        { input: 'double word', result: false }
-      ]
-
-      testData.forEach(({ input, result }) => {
-        it(`should return '${result}' for input = ${input}`, () => {
-          expect(dz6_isWord(input)).toEqual(result);
-        })
-      })
-    });
-
-    describe('Test dz6_pow', () => {
-      /*
-        2.Напишите функцию isWord, которая принимает на
-        вход текстовую строку. Функция возвращает true, если
-        строка состоит из одного слова и false, если из
-        нескольких.
-     */
-      const testData = [
-        { a: 2, b: 3, result: 8 },
-        { a: 100, b: 0, result: 1 }
-      ]
-      testData.forEach(({ a, b, result }) => {
-        it(`should return '${result}' for input = ${a}, ${b}`, () => {
-          expect(dz6_pow(a, b)).toEqual(result);
-        })
-      })
-    });
+    })
   });
-})
 
+  describe('Test dz6_isWord', () => {
+    /*
+        2.Напишите функцию isWord, которая принимает на
+        вход текстовую строку. Функция возвращает true, если
+        строка состоит из одного слова и false, если из
+        нескольких.
+     */
+    const testData = [
+      { input: 'word', result: true },
+      { input: 'double word', result: false }
+    ]
+
+    testData.forEach(({ input, result }) => {
+      it(`should return '${result}' for input = ${input}`, () => {
+        expect(dz6_isWord(input)).toEqual(result);
+      })
+    })
+  });
+
+  describe('Test dz6_pow', () => {
+    /*
+        2.Напишите функцию isWord, которая принимает на
+        вход текстовую строку. Функция возвращает true, если
+        строка состоит из одного слова и false, если из
+        нескольких.
+     */
+    const testData = [
+      { a: 2, b: 3, result: 8 },
+      { a: 100, b: 0, result: 1 }
+    ]
+    testData.forEach(({ a, b, result }) => {
+      it(`should return '${result}' for input = ${a}, ${b}`, () => {
+        expect(dz6_pow(a, b)).toEqual(result);
+      })
+    })
+  });
+});
